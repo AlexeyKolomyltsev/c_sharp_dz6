9625e4e85a1163e1099b773f9b06270bc28e9a9c
AlexeyKolomyltsev <allex_doma@mail.ru>
2022-05-26 21:59:41 +0800

    добавил задачу 1

---
+++ 1_arry_mxn/Program.cs
@@ -0,0 +1,27 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+void PrintArr(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк m = ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите количество столбцов n = ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+double[,] arrayMN = new double[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        arrayMN[i, j] = new Random().NextDouble();
+    }
+}
+
+PrintArr(arrayMN);
