251346e0657a8941aa400dbd81c8c4b6e8446009
AlexeyKolomyltsev <allex_doma@mail.ru>
2022-05-26 22:11:32 +0800

    добавил задачу 2

---
+++ 2_array_Amn/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+void PrintArr(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк m = ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите количество столбцов n = ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+double[,] arrayMN = new double[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        arrayMN[i, j] = i + j;
+    }
+}
+
+PrintArr(arrayMN);
